cfb1760ca6672946f562303bcd48abcc9e2f3126
aureliacitra <citrapangukir@gmail.com>
2025-05-05 18:52:04 +0700

    add library

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+    public:
+    int nim;
+    void showtim()
+    {
+        cout << "No induk = " << nim << endl; 
+    }
+};
+
+int main()
+{}
